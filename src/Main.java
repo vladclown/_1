881143afd5fe99122f0d1b6640a95bb26df1dc96
Tichinskiy Vladislav <whiteknight19283@gmail.com>
2023-02-15 22:55:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String firstString = "Hello";
+        String secondString = "Alexandra Abramov";
+
+        System.out.println(firstString + System.lineSeparator() + secondString);
+
+    }
+}
